2f12040b4ecbd1ba1df4a9f0c87bc086d9141971
trungthanh123 <trungthanh.uit.workshop@gmail.com>
2017-05-02 21:46:23 +0700

    second commit

---
+++ js/js.js
@@ -0,0 +1,48 @@
+// $(document).ready(function(){
+//   $(".navbar-2").affix({
+//     offset: {top: 544}
+//   });
+// });
+
+var  affixElement = '#menu';
+$(document).ready(function(){
+  $(affixElement).affix({
+    offset: {
+      top: function () {
+        return ( this.top = $(affixElement).offset().top)
+      },
+      bottom: function () { 
+        return (this.bottom = $('#footer').outerHeight(true))
+      }
+    }
+  });
+}); //./ready
+
+
+
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 60
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
